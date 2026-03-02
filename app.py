014365512c80217bcef783b81080a8cb3346f864
PaoBarreto99 <paolalinab@gmail.com>
2026-03-02 11:09:04 -0300

    app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import streamlit.components.v1 as components
 # Leer CSV fijo del repo
 df = pd.read_csv("certificaciones.csv", encoding="latin-1")
 
+# Si usás tu Dashboard.html
+with open("Dashboard.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
 # Mostrar algunos datos (opcional)
 st.write("Vista previa de datos")
 st.dataframe(df)
 
-# Si usás tu Dashboard.html
-with open("Dashboard.html", "r", encoding="utf-8") as f:
-    html_code = f.read()
-
 components.html(html_code, height=800, scrolling=True)
+
 
 
 
