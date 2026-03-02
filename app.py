d0e13fae37f12490e0245a55ef767e17329943da
PaoBarreto99 <paolalinab@gmail.com>
2026-03-02 11:09:24 -0300

    app.py

--- app.py
+++ app.py
@@ -8,11 +8,9 @@ df = pd.read_csv("certificaciones.csv", encoding="latin-1")
 # Si usás tu Dashboard.html
 with open("Dashboard.html", "r", encoding="utf-8") as f:
     html_code = f.read()
-# Mostrar algunos datos (opcional)
-st.write("Vista previa de datos")
-st.dataframe(df)
 
 components.html(html_code, height=800, scrolling=True)
+
 
 
 
