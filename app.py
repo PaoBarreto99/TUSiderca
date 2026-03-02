4cb940abbe6e7b679910b1d564060465082b634b
PaoBarreto99 <paolalinab@gmail.com>
2026-03-02 17:22:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,21 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
+# 👇 Oculta el título negro
+st.markdown("""
+<style>
+h1 { display: none; }
+</style>
+""", unsafe_allow_html=True)
+
 df = pd.read_csv("certificaciones.csv", encoding="latin-1")
+
+df.rename(columns={
+    "Fecha_vencimiento": "Fecha_Vencimiento",
+    "Fecha_certificacion": "Fecha_Certificacion"
+}, inplace=True)
+
+df["Fecha_Vencimiento"] = pd.to_datetime(df["Fecha_Vencimiento"], errors="coerce")
 
 csv_string = df.to_csv(index=False)
 
