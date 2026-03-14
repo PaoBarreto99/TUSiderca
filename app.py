d0ce64e49d7828fd1ab38b4201950908cf816368
PaoBarreto99 <paolalinab@gmail.com>
2026-03-14 14:59:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,32 +6,36 @@ st.set_page_config(layout="wide")
 
 st.markdown("""
 <style>
+
 .block-container {
-padding-top:0rem !important;
-padding-bottom:0rem !important;
-padding-left:0rem !important;
-padding-right:0rem !important;
+padding-top: 0rem !important;
+padding-bottom: 0rem !important;
+padding-left: 0rem !important;
+padding-right: 0rem !important;
 }
+
+.main {padding:0rem !important;}
+
 header {visibility:hidden;}
 footer {visibility:hidden;}
+
 </style>
 """, unsafe_allow_html=True)
 
-df = pd.read_csv("certificaciones.csv",encoding="latin-1")
 
-df.rename(columns={
-"Fecha_vencimiento":"Fecha_Vencimiento",
-"Fecha_certificacion":"Fecha_Certificacion"
-},inplace=True)
+# Leer CSV
+df = pd.read_csv("certificaciones.csv", encoding="latin-1")
 
-csv_string=df.to_csv(index=False)
+csv_string = df.to_csv(index=False)
 
-with open("Dashboard.html","r",encoding="utf-8") as f:
-    html_code=f.read()
 
-html_code=html_code.replace(
+# Cargar HTML
+with open("Dashboard.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
+
+html_code = html_code.replace(
 "</head>",
 f"<script>window.csvData=`{csv_string}`;</script></head>"
 )
 
-components.html(html_code,height=1500,scrolling=True)
+components.html(html_code, height=1400, scrolling=True)
